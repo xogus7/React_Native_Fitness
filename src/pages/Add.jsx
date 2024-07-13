1b35a4f2846b19af5766b2edc49b4f77ef4066c5
xogus7 <xogus2676@gmail.com>
2024-07-14 07:35:05 +0900

    feat: PermissionsAndroid

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, Text, Image, FlatList, TouchableOpacity, useWindowDimensions} from 'react-native';
+import { SafeAreaView, View, Text, Image, FlatList, TouchableOpacity,
+  useWindowDimensions, Platform, PermissionsAndroid} from 'react-native';
 import { CameraRoll } from '@react-native-camera-roll/camera-roll';
 
 const Add = ({ navigation }) => {
@@ -13,7 +14,49 @@ const Add = ({ navigation }) => {
         FetchImages();
     }, [])
 
+    async function hasAndroidPermission() {
+        const getCheckPermissionPromise = () => {
+          if (Platform.Version >= 33) {
+            return Promise.all([
+              PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES),
+              PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO),
+            ]).then(
+              ([hasReadMediaImagesPermission, hasReadMediaVideoPermission]) =>
+                hasReadMediaImagesPermission && hasReadMediaVideoPermission,
+            );
+          } else {
+            return PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE);
+          }
+        };
+      
+        const hasPermission = await getCheckPermissionPromise();
+        if (hasPermission) {
+          return true;
+        }
+        const getRequestPermissionPromise = () => {
+          if (Platform.Version >= 33) {
+            return PermissionsAndroid.requestMultiple([
+              PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES,
+              PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO,
+            ]).then(
+              (statuses) =>
+                statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_IMAGES] ===
+                  PermissionsAndroid.RESULTS.GRANTED &&
+                statuses[PermissionsAndroid.PERMISSIONS.READ_MEDIA_VIDEO] ===
+                  PermissionsAndroid.RESULTS.GRANTED,
+            );
+          } else {
+            return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE).then((status) => status === PermissionsAndroid.RESULTS.GRANTED);
+          }
+        };
+      
+        return await getRequestPermissionPromise();
+      }
+
     const FetchImages = async () => {
+        if (Platform.OS === "android" && !(await hasAndroidPermission())) {
+            console.log("fail");
+          }
         CameraRoll.getPhotos({
             first: 100,
             assetType: 'Photos',
