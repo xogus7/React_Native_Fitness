ab9c2146584bb18a404281384b37c4cb219a9844
xogus7 <xogus2676@gmail.com>
2024-07-16 23:57:31 +0900

    refactor: 절대경로

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@icons': './src/assets/icons',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
